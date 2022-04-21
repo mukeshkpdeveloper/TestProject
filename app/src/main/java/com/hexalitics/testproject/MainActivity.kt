87a30c603fb21927683d7b6742ccaf829337e070
sumit-android-dev <Sumit.ojha@hexalitics.com>
2022-04-21 18:07:08 +0530

    Bottom navigation

--- app/src/main/java/com/hexalitics/testproject/MainActivity.kt
+++ app/src/main/java/com/hexalitics/testproject/MainActivity.kt
@@ -1,18 +1,28 @@
 package com.hexalitics.testproject
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 import com.google.android.material.shape.CornerFamily
 import com.google.android.material.shape.MaterialShapeDrawable
 import com.google.android.material.shape.ShapeAppearanceModel
+import com.hexalitics.testproject.databinding.ActivityMainBinding
+import com.hexalitics.testproject.databinding.ActivitySplashBinding
 import com.hexalitics.testproject.util.TicketEdgeTreatment
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var mContext: Context
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-
+        binding.bottomNavigationView.background = null
+        binding.bottomNavigationView.menu.getItem(2).isEnabled = false
 
     }
 }
